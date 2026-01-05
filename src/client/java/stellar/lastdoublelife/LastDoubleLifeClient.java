9830d14b5581ecdcbb4a85bfb0d717f0cd58710e
Merlijn <44466724+MBrinks2239@users.noreply.github.com>
2026-01-05 15:48:55 +0100

    Template init

---
+++ src/client/java/stellar/lastdoublelife/LastDoubleLifeClient.java
@@ -0,0 +1,10 @@
+package stellar.lastdoublelife;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LastDoubleLifeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
